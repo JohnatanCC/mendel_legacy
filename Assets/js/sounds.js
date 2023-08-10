fc9e0f60d0b77320985a703432383c85acdbc14d
JohnatanCC <johnatan.cavalcante.costa@gmail.com>
2023-08-10 20:43:46 -0300

    Implementação da musica

--- Assets/js/sounds.js
+++ Assets/js/sounds.js
@@ -8,5 +8,45 @@ specificInputs.forEach((input) => {
   });
 });
 
-clickSound.volume = 0.3;
-errorSound.volume = 0.5;
+//Music
+
+const audioElement = document.getElementById('audioElement');
+let volume = 1.0; // Valor inicial do volume
+const volumeDisplay = document.getElementById('volumeDisplay');
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'm' || event.key === 'M') {
+    audioElement.play();
+  } else if (event.key === 's' || event.key === 'S') {
+    audioElement.pause();
+    audioElement.currentTime = 0;
+  } else if (event.key === '+' || event.key === 'Add') {
+    volume = Math.min(volume + 0.1, 1.0);
+    audioElement.volume = volume;
+    volumeDisplay.textContent = `Volume: ${Math.round(volume * 100)}%`;
+    volumeDisplay.style.opacity = 1;
+    setTimeout(() => {
+      volumeDisplay.style.opacity = 0;
+    }, 1000); // Após 1 segundo, o elemento de exibição de volume desaparece
+  } else if (event.key === '-' || event.key === 'Subtract') {
+    volume = Math.max(volume - 0.1, 0.0);
+    audioElement.volume = volume;
+    volumeDisplay.textContent = `Volume: ${Math.round(volume * 100)}%`;
+    volumeDisplay.style.opacity = 1;
+    setTimeout(() => {
+      volumeDisplay.style.opacity = 0;
+    }, 1000);
+  }
+});
+
+//Mobile
+const playMusic = document.getElementById('playMusic');
+playMusic.addEventListener('click', () => {
+  // Verifica se o áudio já está tocando, pausa se estiver, ou inicia se não estiver
+  if (audioElement.paused) {
+    audioElement.play();
+  } else {
+    audioElement.pause();
+    audioElement.currentTime = 0; // Reinicia a reprodução para o início
+  }
+});
