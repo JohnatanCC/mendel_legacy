607bf8fd6120c584c1fdc0d3e4776325464878c8
JohnatanCC <johnatan.cavalcante.costa@gmail.com>
2023-08-10 00:49:09 -0300

    Parte da teoria adicionada

--- Assets/js/theory.js
+++ Assets/js/theory.js
@@ -16,27 +16,36 @@ const sliders = [
   {
     title: "O que é génetica?",
     content:
-      "A genética é o estudo da hereditariedade e da variação dos organismos vivos. Ela explora como as características são passadas de uma geração para outra, bem  como as diferenças genéticas podem levar a variações individuais e até mesmo a doenças. A genética também está envolvida em muitas áreas da biologia, incluindo evolução, biotecnologia e medicina. Os genes são a unidade básica da hereditariedade e são compostos de DNA, que contém as instruções para a produção de proteínas. A genética moderna usa ferramentas como sequenciamento de DNA e edição de genes para estudar e manipular o material genético de organismos vivos. ",
-    img: "Assets/imgs/theory/img1.jpg",
+      "A genética é a área da biologia que estuda os fenômenos da hereditariedade, podendo ser usada em investigação de paternidade, soluções de crimes, elucidação de grau de parentesco entre espécies, desenvolvimento de vacinas e aconselhamento genético.      ",
+    img: "Assets/imgs/sticker-practice.png",
   },
   {
-    title: "A teoria de mendel",
+    title: "Gregor Mendel",
     content:
-      "A teoria de Mendel, também conhecida como leis da hereditariedade de Mendel, foi desenvolvida  pelo monge e botânico austríaco Gregor Mendel no século XIX. Ele realizou experimentos com ervilhas e formulou três leis fundamentais da hereditariedade: <br> <br>  1.Lei da Segregação: os dois alelos de um gene segregam-se durante a formação dos gametas, de modo que cada gameta recebe apenas um dos alelos.  <br>   <br>   2.Lei da Dominância: quando dois alelos diferentes estão presentes em um indivíduo, um deles (o alelo dominante) mascara o efeito do outro (o alelo recessivo). <br> <br> 3.Lei da Distribuição Independente: alelos de diferentes genes segregam-se independentemente uns dos outros durante a formação dos gametas, de modo que a presença de um alelo em um gene não afeta a presença de um alelo em outro gene.",
-    img: "Assets/imgs/theory/img3.png",
+      "Os estudos de Mendel sobre as ervilhas envolveram conhecimentos de biologia e matemática. A ervilha foi usada como material de estudo por ter ciclo de vida curto, ser fácil de cultivar, ter características contrastantes e autofecundação.      ",
+    img: "Assets/imgs/sticker-practice.png",
   },
   {
-    title: "Gene,Alelo,Fenótipo e Genótipo",
+    title: "A Metodologia de Mendel - parte I ",
     content:
-      "Essas leis explicam como as características são herdadas de uma geração para outra e por que algumas características são mais comuns em uma população do que em outra. Além das leis fundamentais da hereditariedade, a teoria de Mendel também introduziu os conceitos de gene, alelo, fenótipo e genótipo. <br> <br> Gene: é uma unidade hereditária que contém a informação genética para a produção de uma proteína ou outra molécula funcional. <br> <br> Alelo: é uma das diferentes formas de um gene que podem existir em um locus cromossômico específico.<br> <br> Fenótipo: é a aparência física ou características observáveis de um organismo, que são determinadas pela interação entre o genótipo e o ambiente.<br> <br> Genótipo: é a composição genética de um organismo, ou seja, os alelos que estão presentes em seus cromossomos.",
-    img: "Assets/imgs/theory/img4.webp",
+      "<p>Mendel realizou cruzamentos entre plantas de linhagem pura com características contrastantes (Parental ou P), sendo uma com sementes verdes e a outra com sementes amarelas, gerando assim a Geração F1, composta 100% com sementes amarelas.</p><br><p>Após isso, foi realizada uma autofecundação, gerando a Geração F2, sendo composta 75% com sementes amarelas e 25% com sementes verdes.</p><br><p><bold>1º Lei de Mendel</bold></p><br><p>Cada característica hereditária é determinada por 2 fatores, um herdado do genitor materno e outro do genitor paterno; os fatores de cada par separam-se no momento da formação dos gametas.</p>",
+    img: "Assets/imgs/sticker-practice.png",
   },
   {
-    title: "Experimentos com Ervilhas",
+    title: "Cruzamento Teste",
     content:
-      "Gregor Mendel conduziu seus experimentos utilizando ervilhas pelos seguintes motivos: <br> <br>•  Planta de fácil cultivo e desenvolvimento em curto período;<Br>• Produção de muitas sementes;<br>• Rápido ciclo reprodutivo;<br>• Facilidade de controlar a fecundação das plantas; <br>• Capacidade de realizar autofecundação. <br> <br> <br>Os seus experimentos analisaram sete características das ervilhas: cor da flor, posição da flor no caule, cor da semente, textura da semente, forma da vagem, cor da vagem e altura da planta.",
-    img: "Assets/imgs/theory/img2.jpg",
+      "<p>Quando observamos um indivíduo com característica dominante é impossível saber se ele é homozigótico dominante(BB) ou heterozigótico(Bb), para ter certeza é necessário que faça um cruzamento com um outro indivíduo com característica recessiva(bb), caso os descendentes sejam 100% com características dominantes, o indivíduo é homozigótico dominante, ou seja, puro, mas caso os descendentes sejam 50% com características recessivas, o indivíduo é heterozigótico.</p>      ",
+    img: "Assets/imgs/sticker-practice.png",
+    
   },
+  {
+    title: "A Metodologia de Mendel - parte II ",
+    content:
+      "<p>Mendel continuou estudando as ervilhas, usando a mesma metodologia, fez cruzamentos para observação da transmissão de duas ou mais características, como cor e textura. Sendo “V” para cor e “R” para textura, após isso foi gerado o seguinte resultado:</p><br><p>1.Geração P: Amarela Lisa(VVRR) X Verde Rugosa(vvrr);</p><br><p>2.Geração F1: 100% Amarela Lisa(VvRr);</p><br><p>Geração F2: 25% Amarela Lisa(V_R_), 25% Amarela Rugosa(V_rr), 25% Verde Lisa(vvR_), 25% Verde Rugosa(vvrr).</p>      ",
+    img: "Assets/imgs/sticker-practice.png",
+    
+  },
+  
 ];
 
 //current slide
@@ -65,7 +74,7 @@ prevBtn.addEventListener("click", () => {
 let displaySlide = () => {
   slideContainer.innerHTML = `
   <div class="theory-area">
-        <div class="gap-20 d-centered">
+        <div class="gap-20 d-centered-no-align">
        <div class="img-fluid">
           <img src="${sliders[i].img}" alt="image" />
        </div>
