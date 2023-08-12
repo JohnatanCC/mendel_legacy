475ec2f1d6e9bdbdba74c31ee08c5a973527d5b6
JohnatanCC <johnatan.cavalcante.costa@gmail.com>
2023-08-11 23:36:19 -0300

    Criação da nova tela de quiz

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,15 +1,5 @@
 let questions = [
-    {
-        question: '1 - O sucesso dos experimentos de Mendel está diretamente ligado ao material escolhido para sua pesquisa: ervilhas. Analise as alternativas abaixo e marque a única que não representa uma vantagem do uso dessa espécie.',
-        options: [
-            ' Fácil cultivo',
-            'Produz poucos descendentes',
-            'Ciclo de vida curto',
-            ' Facilidade de polinização artificial',
-            'Possui muitas variedades'
-        ],
-        answer: 2
-    },
+ 
     {
         question: '2 - Em um experimento, ao cruzar plantas puras de flores roxas com plantas puras de flores brancas, obteve-se 100% de plantas com flores roxas em F1. Levando em consideração que o experimento obedece à Primeira Lei de Mendel, espera-se que em F2 as flores roxas e brancas apresentem-se em uma proporção de',
         options: [
@@ -19,7 +9,20 @@ let questions = [
             '3:1',
             '2:5'
         ],
-        answer: 4
+        answer: 4,
+        difficulty: 'medium'
+    },
+    {
+        question: '1 - O sucesso dos experimentos de Mendel está diretamente ligado ao material escolhido para sua pesquisa: ervilhas. Analise as alternativas abaixo e marque a única que não representa uma vantagem do uso dessa espécie.',
+        options: [
+            ' Fácil cultivo',
+            'Produz poucos descendentes',
+            'Ciclo de vida curto',
+            ' Facilidade de polinização artificial',
+            'Possui muitas variedades'
+        ],
+        answer: 2,
+        difficulty: 'easy'
     },
     {
         question: '3 - Ao realizar seus experimentos com ervilhas, Mendel cruzou plantas com sementes amarelas e verdes, obtendo, em F1, 100% das plantas com sementes amarelas. Em F2, obteve 75% das plantas com sementes amarelas e 25% de plantas com sementes verdes. Podemos concluir, portanto, que em F1 temos indivíduos',
@@ -30,7 +33,8 @@ let questions = [
             'Puros dominantes',
             'Puros recessivos'
         ],
-        answer: 3
+        answer: 3,
+        difficulty: 'medium'
     },
     {
         question: '<p>4 - Com relação à primeira lei de Mendel, analise as proposições abaixo.</p><br><p>1. Cada característica de um organismo é condicionada por dois fatores (genes alelos), um proveniente do pai e outro da mãe.</p><br><p>2. Os dois fatores do par (ou alelos) são transmitidos para cada gameta, no momento da sua formação.</p><br><p>3. Com a união dos gametas na fecundação, o par de alelos para cada característica é reconstituído.</p><br><p>4. No momento da formação dos gametas, os alelos se separam indo apenas um para cada gameta.</p><br>5. Um alelo do par é transmitido, dependente da sua manifestação.</p><br>Estão corretas, apenas:',
@@ -41,7 +45,8 @@ let questions = [
             '1, 4 e 5',
             '1, 2 e 3'
         ],
-        answer: 3
+        answer: 3,
+        difficulty: 'hard'
     },
     {
         question: '<p>5 - (Fuvest) Considere os seguintes cruzamentos para ervilha, sabendo que V representa o gene que determina cor amarela dos cotilédones e é dominante sobre o alelo v, que determina cor verde.</p><br><p>I. VV x vv<br>II. Vv x Vv</p><br><p>III. Vv x vv</p><br>Um pé de ervilha, heterozigoto e que, portanto, pode produzir vagens com sementes amarelas e com sementes verdes, pode resultar:',
@@ -52,7 +57,19 @@ let questions = [
             'Apenas dos cruzamentos II e III',
             'Dos cruzamentos I, II e III'
         ],
-        answer: 5
+        answer: 5,
+        difficulty: 'hard'
     },
    
 ];
+
+// Função para embaralhar as questões aleatoriamente
+// function shuffleArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+// }
+
+// Embaralhar as questões aleatoriamente
+// shuffleArray(questions);
